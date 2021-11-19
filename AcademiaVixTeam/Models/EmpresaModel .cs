a48034555cacdbc7038b78dd786804dad6c389e4
Victor Matheus dos Santos Pereira <victormspereira@hotmail.com>
2021-11-19 12:42:29 -0300

    Atualizacao

--- AcademiaVixTeam/Models/EmpresaModel .cs
+++ AcademiaVixTeam/Models/EmpresaModel .cs
@@ -6,7 +6,11 @@ namespace NovoProj.Models
 
         public string? Nome { get; set; }
 
+        public bool ShowNome => !string.IsNullOrEmpty(Nome);
+
         public string? nomeFantasia { get; set; }
+
+        public bool ShownomeFantasia => !string.IsNullOrEmpty(nomeFantasia);
 
         public int cnpj { get; set; }
     }
